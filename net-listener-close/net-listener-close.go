90721e132b286350e36bcded6186679ef0a1807c
Eduard Urbach <e.urbach@gmail.com>
2017-10-31 06:27:34 +0100

    Added net-listener-close

---
+++ net-listener-close/net-listener-close.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	i := 0
+
+	for {
+		listener, err := net.Listen("tcp", ":3000")
+
+		if err != nil {
+			fmt.Printf("failure @ iteration %d | %s\n", i, err)
+			continue
+		}
+
+		go accept(listener)
+		listener.Close()
+
+		i++
+	}
+}
+
+func accept(listener net.Listener) {
+	for {
+		_, err := listener.Accept()
+
+		if err != nil {
+			break
+		}
+	}
+}
