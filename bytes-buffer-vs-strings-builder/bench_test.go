2450d247b16b6f1e0d06e6225dba4e25c053d122
Eduard Urbach <e.urbach@gmail.com>
2019-04-30 18:55:28 +0900

    Improved benchmarks

--- bytes-buffer-vs-strings-builder/bench_test.go
+++ bytes-buffer-vs-strings-builder/bench_test.go
@@ -7,24 +7,55 @@ import (
 	"testing"
 )
 
-func BenchmarkA(b *testing.B) {
+var (
+	_1 = "unknown string"
+	_2 = "another string"
+	_3 = "yet another string"
+	_4 = "oh good lord yet another string"
+)
+
+func BenchmarkBuilderWriteString(b *testing.B) {
 	b.ReportAllocs()
 
 	for n := 0; n < b.N; n++ {
 		var builder strings.Builder
-		fmt.Fprintf(&builder, "%d...%s", 13, "Hello")
-		builder.WriteString("EOL")
+		builder.WriteString(_1)
+		builder.WriteString(_2)
+		builder.WriteString(_3)
+		builder.WriteString(_4)
 		_ = builder.String()
 	}
 }
 
-func BenchmarkB(b *testing.B) {
+func BenchmarkBuilderFprintf(b *testing.B) {
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		var builder strings.Builder
+		fmt.Fprintf(&builder, "%s%s%s%s", _1, _2, _3, _4)
+		_ = builder.String()
+	}
+}
+
+func BenchmarkBufferWriteString(b *testing.B) {
 	b.ReportAllocs()
 
 	for n := 0; n < b.N; n++ {
 		var buffer bytes.Buffer
-		fmt.Fprintf(&buffer, "%d...%s", 13, "Hello")
-		buffer.WriteString("EOL")
+		buffer.WriteString(_1)
+		buffer.WriteString(_2)
+		buffer.WriteString(_3)
+		buffer.WriteString(_4)
+		_ = buffer.String()
+	}
+}
+
+func BenchmarkBufferFprintf(b *testing.B) {
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		var buffer bytes.Buffer
+		fmt.Fprintf(&buffer, "%s%s%s%s", _1, _2, _3, _4)
 		_ = buffer.String()
 	}
 }
