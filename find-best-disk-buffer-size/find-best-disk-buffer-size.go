49da399bcc6975839985c6d504af85fc1650a344
Eduard Urbach <e.urbach@gmail.com>
2019-12-11 15:06:55 +0900

    Added step size

--- find-best-disk-buffer-size/find-best-disk-buffer-size.go
+++ find-best-disk-buffer-size/find-best-disk-buffer-size.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"math"
@@ -10,11 +11,27 @@ import (
 )
 
 func main() {
+	from := int64(1)
+	to := int64(65536)
+	step := int64(1)
+
+	flag.Int64Var(&from, "from", from, "First buffer size")
+	flag.Int64Var(&to, "to", to, "Last buffer size")
+	flag.Int64Var(&step, "step", step, "Step size")
+	flag.Parse()
+
 	lowestSize := int64(math.MaxInt64)
 	lowestTime := int64(math.MaxInt64)
+	file, err := os.Open("moby-dick-1st-chapter.txt")
 
-	for i := int64(1); i <= 65536; i++ {
-		file, err := os.Open("moby-dick-1st-chapter.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	for i := from; i <= to; i += step {
+		_, err := file.Seek(0, 0)
 
 		if err != nil {
 			panic(err)
@@ -36,7 +53,6 @@ func main() {
 		}
 
 		elapsed := time.Now().UnixNano() - start
-		file.Close()
 		fmt.Printf("[%d] %d ns\n", i, elapsed)
 
 		if elapsed < lowestTime {
@@ -44,7 +60,6 @@ func main() {
 			lowestSize = i
 		}
 
-		file = nil
 		buffer = nil
 		runtime.GC()
 	}
